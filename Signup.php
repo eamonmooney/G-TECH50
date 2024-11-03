2ff838f5d4fe68639ae723bc9b6f26940caf8a48
Rayyan1313 <230074804@aston.ac.uk>
2024-11-03 20:12:21 +0000

    Create Signup.php

---
+++ Signup.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <title>Signup Page</title>
+
+<style>
+
+body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            min-height: 100vh;
+            margin: 0;
+
+}
+
+
+
+
+
+
+
+
+</style>
+
+
+
+
+
+
+
+</head>
+
+
+
+<body>
+    <form id="sign-up">
+        <label for="name">Full Name:</label>
+        <input type ="text" name="name" placeholder="Full Name" required/>
+    </br></br>  
+        <label for="dob">Date of Birth:</label>
+        <input type="date" id="dob" name="dob">
+    </br></br>
+        <label for="email">Email:</label>
+        <input type="email" name="email" placeholder="Email" required/>
+    </br></br>  
+        <label for="mobNumber">Mobile Number:</label>
+        <input type="tel" name="mobNumber" placeholder="Mobile Number" required/>
+    </br></br>  
+        <label for="address1">Address Line 1:</label>
+        <input type ="text" name="address1" placeholder="Address Line 1" required/>
+    </br></br>  
+        <label for="address2">Address Line 2:</label>
+        <input type ="text" name="address2" placeholder="Address Line 2"/>
+    </br></br>
+        <label for="postcode">Postcode:</label>
+        <input type ="text" name="postcode" placeholder="Postcode" required/>
+    </br></br>  
+        <input type= "submit" name= "Submit" />
+</form>
+
+
+
+
+
+</body>
+
+
+
+
+
+
+
+
+
+</html>
