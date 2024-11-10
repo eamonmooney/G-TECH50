9f9ad5ef0b10252b5b6bfc3af7fd21e0129978cf
eamonmooney <eamonmooney.ea@gmail.com>
2024-11-10 07:09:14 +0000

    Database connection

---
+++ contactForm.php
@@ -0,0 +1,10 @@
+<?php
+
+//Register button pressed
+if (isset($_POST['submitted'])){
+
+    //Database connection
+    require_once('connectdb.php');
+
+    //INCOMPLETE - Will collect variables from the form submission for SQL Query.
+}
