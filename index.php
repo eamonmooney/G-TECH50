967f9295f67cefb3dd81906d801ecea346bcd431
Tobz112 <tobiassingh21@gmail.com>
2024-11-13 12:38:44 +0000

    Add files via upload

--- index.php
+++ index.php
@@ -19,18 +19,15 @@
       left: 0;
       z-index: 1000;
       padding: 0 20px;
-      
     }
 
     .logo {
       flex: 1;
-       
     }
 
     .nav-links {
       display: flex;
       gap: 10px;
-      
     }
 
     .navbar a {  
@@ -53,7 +50,80 @@
       flex: 1; 
       display: flex;
       justify-content: flex-end;
-      margin-right: 20px; 
+      align-items: center;
+      gap: 5px; 
+      margin-right: 20px;
+    }
+
+    
+    .search-bar {
+      display: flex;
+      align-items: center;
+      background-color: #201c1c;
+      border-radius: 5px;
+      padding: 5px 10px;
+    }
+
+    .search-bar input[type="text"] {
+      background-color: transparent;
+      border: none;
+      color: white;
+      outline: none;
+      padding: 5px;
+      font-size: 16px;
+      width: 120px; 
+    }
+
+    .search-bar .material-icons {
+      color: white;
+      cursor: pointer;
+      font-size: 24px;
+    }
+
+    .search-bar input[type="text"]::placeholder {
+      color: #aaa; 
+    }
+
+    
+    .dropdown {
+      position: relative;
+      margin-top: 15px; 
+    }
+
+    .dropdown-content {
+      display: none;
+      position: absolute;
+      top: 115%;
+      left: 50%;
+      transform: translateX(-50%);
+      background-color: #201c1c;
+      padding: 10px;
+      width: 600px;
+      border-radius: 5px;
+      box-shadow: 0px 8px 16px rgba(0, 0, 0, 0.2);
+    }
+
+    .dropdown:hover .dropdown-content {
+      display: flex;
+      justify-content: center;
+      gap: 5px;
+    }
+
+    .dropdown-content a {
+      color: white;
+      width: 100px;
+      height: 40px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      text-decoration: none;
+      text-align: center;
+      background-color: #201c1c;
+      border-radius: 5px;
+    }
+
+    .dropdown-content a:hover {
+      background-color: #333333;
     }
   </style>
 
@@ -65,16 +135,35 @@
     <div class="logo">
       <a href="Home">Logo</a>
     </div>
+
     <div class="nav-links">
-      <a href="index.php">Home</a>
-      <a href="Categories">Categories</a>
-      <a href="deals.php">Deals</a>
-      <a href="support.php">Support</a>
+      <a href="Home.html">Home</a>
+
+      <div class="dropdown">
+        <a href="Categories">Categories</a>
+
+        <div class="dropdown-content">
+          <a href="Mice.html">Mice</a>
+          <a href="Monitors.html">Monitors</a>
+          <a href="Headphones.html">Headphones</a>
+          <a href="Keyboards.html">Keyboards</a>
+          <a href="Mousepads.html">Mousepads</a>
+        </div>
+
+      </div>
+      <a href="Deals.html">Deals</a>
+      <a href="Support.html">Support</a>
     </div>
+
     <div class="icon-container">
-        <a href="#"><i class="material-icons">search</i></a>
-        <a href="profile.php"><i class="material-icons">account_circle</i></a>
-        <a href="basket.php"><i class="material-icons">shopping_basket</i></a>
+        
+        <div class="search-bar">
+          <input type="text" placeholder="Search...">
+          <i class="material-icons">search</i>
+        </div>
+
+        <a href="profile.html"><i class="material-icons">account_circle</i></a>
+        <a href="basket.html"><i class="material-icons">shopping_basket</i></a>
     </div>
   </div>
 
