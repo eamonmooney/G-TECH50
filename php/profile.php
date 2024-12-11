74985d6e39cf400c81efff13ffad7fbbc3a6de88
eamonmooney <eamonmooney.ea@gmail.com>
2024-12-11 07:47:50 +0000

    Profile info and Order history implementation
    Spent the entire night on this fixing things up
    Changes:
    - Deleted Uneeded SQL comments in my php files
    - Profile page now displays the users information upon loading in
    - Managed to get the order history to actually work and showing important information about the users past orders
    
    Previous commits also involved:
    - Changing how data is collected in the contact form and removing uneeded js variables
    - Changing the search bar so its able to be used in nested folders e.g. product pages

--- php/profile.php
+++ php/profile.php
@@ -25,70 +25,19 @@ try {
     $stmt->execute([$userId]);
     $username = $stmt->fetchColumn();
 
-    // Prepare the query to count the orders for the specific user
-    $stmt = $db->prepare("SELECT COUNT(*) FROM Orders WHERE UserID = ? limit 1");
+    // Query database to get the email through the userId
+    $stmt = $db->prepare("SELECT `email` FROM users WHERE UserID = ? limit 1");
     $stmt->execute([$userId]);
-    $totalOrders = $stmt->fetchColumn();
+    $email = $stmt->fetchColumn();
 
-    // Prepare the query to count the returns for the specific user
-    $stmt = $db->prepare("SELECT COUNT(*) FROM `returns` WHERE UserID = ? limit 1");
-    $stmt->execute([$userId]);
-    $totalReturns = $stmt->fetchColumn();
+    $newHtml = "";
 
-    // Read the HTML file
-    $html = file_get_contents('profile.html');
-
-    var_dump($totalOrders);
-    var_dump($username);
-    
-    //Replace the total orders placeholder
-    $html = str_replace('{{ORDERTOTAL}}', $totalOrders, $html);
-
-    //Display username
-    $html = str_replace('{{USERNAME}}', $username, $html);
-
-    //Display email
-    $html = str_replace('{{USEREMAIL}}', "myemail@mail.com", $html);
-
-    echo $totalOrders;
-    echo $username;
+    $newHtml .= "<h2>Account Information</h2>";
+    $newHtml .= "<p><strong>Username: $username </strong></p>";
+    $newHtml .= "<p><strong>Email: $email </strong></p>";
+    echo $newHtml;
 
 } catch (PDOException $e) {
     echo "Error: " . $e->getMessage();
 }
-
-/*
-
-    CREATE TABLE Users (
-            UserID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-            RoleID INT NOT NULL,
-            Name CHAR(50) NOT NULL,
-            Email CHAR(50) UNIQUE NOT NULL,
-            Password CHAR(50) NOT NULL,
-            FOREIGN KEY (RoleID) REFERENCES Role(RoleID)
-        );
-
-    CREATE TABLE Orders (
-            OrderID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-            UserID INT NOT NULL,
-            OrderTypeID CHAR(10) NOT NULL,
-            OrderDate STRING NOT NULL,
-            OrderCost FLOAT NOT NULL,
-            FOREIGN KEY (UserID) REFERENCES Users(UserID),
-            FOREIGN KEY (OrderTypeID) REFERENCES OrderTypes(OrderID)
-        );
-
-    CREATE TABLE Returns (
-            ReturnID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-            UserID INT NOT NULL,
-            OrderID INT NOT NULL,
-            ProductID INT NOT NULL,
-            ReturnValue FLOAT NOT NULL,
-            ReturnDate DATE NOT NULL,
-            FOREIGN KEY (UserID) REFERENCES Users(UserID),
-            FOREIGN KEY (OrderID) REFERENCES Orders(OrderID),
-            FOREIGN KEY (ProductID) REFERENCES Products(ProductID)
-        );
-
-*/
 ?>
