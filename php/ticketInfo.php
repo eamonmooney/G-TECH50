545e30017d67bc9ee6a47016b46a3cf3af1f4e11
eamonmooney <eamonmooney.ea@gmail.com>
2025-03-14 19:39:06 +0000

    Now collects and displays all logged in users tickets

--- php/ticketInfo.php
+++ php/ticketInfo.php
@@ -10,7 +10,24 @@ require_once('connectdb.php');
 try {
 
     // Prepare a query to get all tickets
-    $stmt = $db->prepare("SELECT TicketID, UserID, Telephone, TicketDate, Closed, TicketContent FROM supporttickets");
+    //$stmt = $db->prepare("SELECT TicketID, UserID, Telephone, TicketDate, Closed, TicketContent FROM supporttickets");
+    //$stmt->execute();
+
+    // Prepare query with JOIN to get username
+    $stmt = $db->prepare("
+        SELECT 
+            supporttickets.TicketID, 
+            supporttickets.UserID, 
+            users.Name, 
+            supporttickets.Telephone, 
+            supporttickets.TicketDate, 
+            supporttickets.Closed, 
+            supporttickets.TicketContent
+        FROM 
+            supporttickets
+        INNER JOIN 
+            users ON supporttickets.UserID = users.UserID
+    ");
     $stmt->execute();
 
     // Fetch all the results
@@ -23,23 +40,20 @@ try {
         foreach ($tickets as $ticket) {
             $newHtml .= "<tr>";
             $newHtml .= "<td>" . htmlspecialchars($ticket['TicketID']) . "</td>";
-
-
-
-            //----- INSERT USERNAME ----
-            $newHtml .= "<td>USERNAME</td>";
-
-
-
+            $newHtml .= "<td>" . htmlspecialchars($ticket['Name']) . "</td>";
             $newHtml .= "<td>" . htmlspecialchars($ticket['Telephone']) . "</td>";
             $newHtml .= "<td>" . htmlspecialchars($ticket['TicketDate']) . "</td>";
             $newHtml .= "<td>" . htmlspecialchars($ticket['Closed']) . "</td>";
             $newHtml .= "<td>" . htmlspecialchars($ticket['TicketContent']) . "</td>";
+            $newHtml .= "<td><button class='close-button'>Close Ticket</button></td>";
             $newHtml .= "</tr>";
         }
     } else {
-        $newHtml .= "No tickets found.";
+        $newHtml .= "<tr><td colspan='7'>No tickets found.</td></tr>";
     }
+
+    echo $newHtml;
+
 } catch (PDOException $e) {
     echo "Error: " . $e->getMessage();
 }
