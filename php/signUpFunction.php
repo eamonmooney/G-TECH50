3623ed7f6f56728948c3e33f003df9d4718f9941
77NOVA77 <ned.goodman@outlook.com>
2025-03-23 03:56:52 +0000

    Successfully hashed & validated admin codes, and made admin sign up work

--- php/signUpFunction.php
+++ php/signUpFunction.php
@@ -51,26 +51,42 @@ if($_SERVER['REQUEST_METHOD'] == "POST")
     // 
     $res0 = $stmt0->fetchColumn();
     // checks if email is in use, outputting error message if it is
-    if ($res0 > 0 && !empty($Email))
-    {
-        echo "This email is already in use";
-    // checks if the user has left empty boxes
-    }else // enter if email is not in use
-    {
-        // prepare sql statement to insert inputted user data to database
-        $sql1 = "INSERT INTO Users (RoleID, Name, Email, Password) VALUES (:RoleID, :Name, :Email, :Password)";
-        $insertstmt1=$db->prepare($sql1);
-        // execute sql statement replacing placeholders with user data and hashing password for security
-        $insertstmt1->execute([
-            ':RoleID' => $RoleID,
-            ':Name' => $Name, 
-			':Email' => $Email, 
-			':Password' => password_hash($Password, PASSWORD_BCRYPT)
-        ]);
-        // send user to sign in page
-        echo "You have successfully created an account";
-        header("Location: /G-TECH50/signin.html");
-        die;
-    }
+        echo("Test correct");
+        $sql1 = "SELECT * FROM AccessKeys WHERE RoleID = 2";
+        $stmt1=$db->prepare($sql1);
+        $stmt1->execute();
+        $codeData1 = $stmt1->fetch(PDO::FETCH_ASSOC);
+        if (password_verify($_POST['accesscode'], $codeData1['AccessKey'])) {
+            $RoleID = 2;
+        }
+        $sql2 = "SELECT * FROM AccessKeys WHERE RoleID = 3";
+        $stmt2=$db->prepare($sql2);
+        $stmt2->execute();
+        $codeData2 = $stmt2->fetch(PDO::FETCH_ASSOC);
+        if (password_verify($_POST['accesscode'], $codeData2['AccessKey'])) {
+            $RoleID = 3;
+        }
+        $sql3 = "SELECT * FROM AccessKeys WHERE RoleID = 4";
+        $stmt3=$db->prepare($sql3);
+        $stmt3->execute();
+        $codeData3 = $stmt3->fetch(PDO::FETCH_ASSOC);
+        if (password_verify($_POST['accesscode'], $codeData3['AccessKey'])) {
+            $RoleID = 4;
+        }
+    
+    // prepare sql statement to insert inputted user data to database
+    $sql4 = "INSERT INTO Users (RoleID, Name, Email, Password) VALUES (:RoleID, :Name, :Email, :Password)";
+    $insertstmt4=$db->prepare($sql4);
+    // execute sql statement replacing placeholders with user data and hashing password for security
+    $insertstmt4->execute([
+        ':RoleID' => $RoleID,
+        ':Name' => $Name, 
+        ':Email' => $Email, 
+        ':Password' => password_hash($Password, PASSWORD_BCRYPT)
+    ]);
+    // send user to sign in page
+    echo "You have successfully created an account";
+    header("Location: /G-TECH50/signin.html");
+    die;
 }
 ?>
