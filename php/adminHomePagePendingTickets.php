e265ab03da608bb16acd8e9fcfb7a429f90caa6b
77NOVA77 <ned.goodman@outlook.com>
2025-03-23 23:38:29 +0000

    admin home page stock tracking and fixed some comments

--- php/adminHomePagePendingTickets.php
+++ php/adminHomePagePendingTickets.php
@@ -7,19 +7,19 @@ session_start();
 require_once('connectdb.php');
 
 try {
-    // query the number of total users
+    // query all open support tickets
     $sql0 = "SELECT * FROM supporttickets WHERE closed = :bool";
     $stmt = $db->prepare($sql0);
     $stmt->execute([':bool' => false]);
     $tickets = $stmt->fetchAll(PDO::FETCH_ASSOC);
     $numTickets = 0;
     if (count($tickets) > 0) {
-        // define sales as total number of orders
+        // define number of tickets by iterating over every open ticket
         foreach($tickets as $ticket) {
             $numTickets += 1;
         }
     };
-    // output total sales to javascript
+    // output total number of tickets to javascript
     echo $numTickets;
 
 } catch (PDOException $e) {
